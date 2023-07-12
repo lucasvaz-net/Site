0ebc044ea33d1284b7810f9a9d97fb2ad5f6b661
Lucas Vaz <110433221+lucasvaz-net@users.noreply.github.com>
2023-07-12 18:19:38 -0300

    Nova pagina de habilidade e correçoes no formulario de contato

---
+++ Controllers/HabilidadesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Site.Controllers
+{
+    public class HabilidadesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
