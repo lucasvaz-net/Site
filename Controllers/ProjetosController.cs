773faed5b05435d8177ee0fb5e452fb8131cbd5e
Lucas Vaz <110433221+lucasvaz-net@users.noreply.github.com>
2023-07-19 19:24:17 -0300

    conexao com banco de dados

--- Controllers/ProjetosController.cs
+++ Controllers/ProjetosController.cs
@@ -1,12 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Site.Models;
 
 namespace Site.Controllers
 {
     public class ProjetosController : Controller
     {
+        private readonly string _connectionString; // Substitua aqui pela sua string de conexão com o banco de dados
+
+        public ProjetosController()
+        {
+            _connectionString = "Data Source=SQL5085.site4now.net;Initial Catalog=db_a9c2c8_tarefas;User ID=db_a9c2c8_tarefas_admin;Password=Vitoriade10.;";
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Projetos> projetos = new List<Projetos>();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string sqlQuery = "SELECT id, nome, descricao, linkgithub, linkweb, login, senha FROM Projetos";
+
+                SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Projetos projeto = new Projetos
+                    {
+                        id = (int)reader["id"],
+                        nome = reader["nome"].ToString(),
+                        descricao = reader["descricao"].ToString(),
+                        linkgithub = reader["linkgithub"].ToString(),
+                        linkweb = reader["linkweb"].ToString(),
+                        login = reader["login"].ToString(),
+                        senha = reader["senha"].ToString()
+                    };
+
+                    projetos.Add(projeto);
+                }
+
+                reader.Close();
+            }
+
+            return View(projetos);
         }
     }
 }
---
+++ Models/Projetos.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Site.Models
+{
+
+    public class Projetos
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string nome { get; set; }
+
+        [Required]
+        public string descricao { get; set; }
+
+        public string linkgithub { get; set; }
+
+        public string linkweb { get; set; }
+
+        public string login { get; set; }
+
+        public string senha { get; set; }
+
+        // Outros campos relevantes para a exibição no card
+    }
+
+}
