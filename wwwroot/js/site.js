bb39a517ce0df76cd4c29e3cae86bec6e8991f1d
Lucas Vaz <110433221+lucasvaz-net@users.noreply.github.com>
2023-08-02 19:47:46 -0300

    background foi trocado por uma animação css

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function typeWriter(elemento) {
+    const texto = elemento.innerHTML;
+    elemento.innerHTML = '';
+
+    let index = 0;
+
+    const escrever = () => {
+        elemento.innerHTML += texto[index];
+        index++;
+
+        if (index < texto.length) {
+            setTimeout(escrever, 100);
+        } else {
+            exibirBotao(); // Chama a função para exibir o botão após a digitação do texto
+        }
+    };
+
+    escrever();
+}
+
+const titulo = document.querySelector('h1');
+typeWriter(titulo);
+
+
+setTimeout(function () {
+    document.getElementById("button-container").style.opacity = "1";
+}, 13000);
